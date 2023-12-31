70978ebdcddc6226546482b424b006ea2e28caf5
Artturi <kerminenartturi@gmail.com>
2023-12-31 09:55:00 +0200

    Fix player able to jump up through ceilings

--- main.cpp
+++ main.cpp
@@ -68,7 +68,7 @@ typedef struct player
     float x, y;
     float velx, vely;
     bool jump;
-    bool coll[3];
+    bool coll[4];
 } player;
 
 enum Collider {none=0, soft, hard};
@@ -180,8 +180,9 @@ void do_player_collision(player& player, const std::vector<chunk>& chunks)
         bool& col_l = player.coll[0];
         bool& col_b = player.coll[1];
         bool& col_r = player.coll[2];
+        bool& col_t = player.coll[3];
         
-        col_l = col_b = col_r = false;
+        col_l = col_b = col_r = col_t = false;
 
         auto CIndexFromBlock = [](int bx, int by)
         {
@@ -207,9 +208,10 @@ void do_player_collision(player& player, const std::vector<chunk>& chunks)
         // THE BLOCK ON WHICH THE PLAYERS HEAD IS ON
         SDL_Point PHeadBlock {floor(player.x / BLOCK_SIZE), floor(player.y / BLOCK_SIZE)};
 
-        // BOTTOM COLLISION        
+        // BOTTOM & TOP COLLISION        
         {
 
+            // BOTTOM
             for(int k = 0; k < PLAYER_W_MULT * 2; k++) 
             {
                 int actual_loc = k - 1;
@@ -234,6 +236,34 @@ void do_player_collision(player& player, const std::vector<chunk>& chunks)
 
                 SDL_RenderDrawRectF(renderer, &collider);
             }
+
+            // TOP
+            for(int k = 0; k < PLAYER_W_MULT * 2; k++) 
+            {
+                int actual_loc = k - 1;
+                
+                SDL_Point globalCoord {PHeadBlock.x + actual_loc, PHeadBlock.y - 1};
+                SDL_Point chunkCoord = CIndexFromBlock(globalCoord.x, globalCoord.y);
+                
+                int actualChunk = WORLD_CHUNK_W * chunkCoord.y + chunkCoord.x;
+                SDL_Point localCoord = GetLocalBlockPoint(globalCoord.x, globalCoord.y);
+                
+                SDL_FRect collider {
+                    globalCoord.x * BLOCK_SIZE - camera.x,
+                    globalCoord.y * BLOCK_SIZE - camera.y,
+                    BLOCK_SIZE,
+                    BLOCK_SIZE * 1.025f
+                };
+                
+                if(SDL_HasIntersectionF(&collider, &player_col) && 
+                    chunks[actualChunk].arr[localCoord.x][localCoord.y].col 
+                    == Collider::hard)
+                    col_t = true;
+
+                SDL_RenderDrawRectF(renderer, &collider);
+            }
+
+
         }
 
         // LEFT & RIGHT COLLISION
@@ -368,13 +398,14 @@ void do_player_move(player& player, const Uint8* keystate, const float dt)
         bool& lcol = player.coll[0];
         bool& bcol = player.coll[1];
         bool& rcol = player.coll[2];
+        bool& tcol = player.coll[3];
 
         // PLAYER
         if(keystate[SDL_SCANCODE_A] == 1)
             player.velx = -10;
         if(keystate[SDL_SCANCODE_D] == 1)
             player.velx = 10;
-        if(keystate[SDL_SCANCODE_S] == 1 && bcol && !player.jump) 
+        if(keystate[SDL_SCANCODE_S] == 1 && bcol && !tcol && !player.jump) 
         {
             player.vely -= JUMP_FORCE;
             player.jump = true;
@@ -390,6 +421,8 @@ void do_player_move(player& player, const Uint8* keystate, const float dt)
         
         if(player.vely > TERMINAL_VELOCITY)
             player.vely = TERMINAL_VELOCITY;
+        if(tcol)
+            player.vely = GRAVITY * dt;
 
         // Add motion to player
         if(!bcol)

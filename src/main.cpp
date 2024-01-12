0895ecaae3ce8b23c2080c95be40f2e5b3bb4c3c
Artturi <kerminenartturi@gmail.com>
2024-01-12 15:27:34 +0200

    remove unused boilerplate and comments.

--- src/main.cpp
+++ src/main.cpp
@@ -248,53 +248,24 @@ int main(int argc, char* argv[])
     if(!initialize()) 
         SDL_Quit();
 
-    /* GUI */
+    // NUKLEAR NUKLEAR NUKLEAR
     struct nk_context *ctx;
     struct nk_colorf bg;
     float font_scale = 1;
     
-    // NUKLEAR NUKLEAR NUKLEAR
-    
-    /* GUI */
     ctx = nk_sdl_init(window, renderer);
-    /* Load Fonts: if none of these are loaded a default font will be used  */
-    /* Load Cursor: if you uncomment cursor loading please hide the cursor */
     {
         struct nk_font_atlas *atlas;
         struct nk_font_config config = nk_font_config(0);
         struct nk_font *font;
 
-        /* set up the font atlas and add desired font; note that font sizes are
-         * multiplied by font_scale to produce better results at higher DPIs */
         nk_sdl_font_stash_begin(&atlas);
         font = nk_font_atlas_add_default(atlas, 13 * font_scale, &config);
-        /*font = nk_font_atlas_add_from_file(atlas, "../../../extra_font/DroidSans.ttf", 14 * font_scale, &config);*/
-        /*font = nk_font_atlas_add_from_file(atlas, "../../../extra_font/Roboto-Regular.ttf", 16 * font_scale, &config);*/
-        /*font = nk_font_atlas_add_from_file(atlas, "../../../extra_font/kenvector_future_thin.ttf", 13 * font_scale, &config);*/
-        /*font = nk_font_atlas_add_from_file(atlas, "../../../extra_font/ProggyClean.ttf", 12 * font_scale, &config);*/
-        /*font = nk_font_atlas_add_from_file(atlas, "../../../extra_font/ProggyTiny.ttf", 10 * font_scale, &config);*/
-        /*font = nk_font_atlas_add_from_file(atlas, "../../../extra_font/Cousine-Regular.ttf", 13 * font_scale, &config);*/
         nk_sdl_font_stash_end();
 
-        /* this hack makes the font appear to be scaled down to the desired
-         * size and is only necessary when font_scale > 1 */
         font->handle.height /= font_scale;
-        /*nk_style_load_all_cursors(ctx, atlas->cursors);*/
         nk_style_set_font(ctx, &font->handle);
     }
-
-    #ifdef INCLUDE_STYLE
-    /* ease regression testing during Nuklear release process; not needed for anything else */
-    #ifdef STYLE_WHITE
-    set_style(ctx, THEME_WHITE);
-    #elif defined(STYLE_RED)
-    set_style(ctx, THEME_RED);
-    #elif defined(STYLE_BLUE)
-    set_style(ctx, THEME_BLUE);
-    #elif defined(STYLE_DARK)
-    set_style(ctx, THEME_DARK);
-    #endif
-    #endif   
 
     // NUKLEAR NUKLEAR NUKLEAR
 
@@ -378,42 +349,21 @@ int main(int argc, char* argv[])
         }
 
         // CAMERA
-        do_camera_move(player, keystate, dt);
- 
+        do_camera_move(player, keystate, dt); 
 
         // GUI GUI GUI
         
         /* GUI */
-        if (nk_begin(ctx, "Demo", nk_rect(50, 50, 230, 250),
-            NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|
-            NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE))
+        if (nk_begin(ctx, "Inventory", nk_rect(20, 20, 500, 300),
+            NK_WINDOW_BORDER))
         {
             enum {EASY, HARD};
             static int op = EASY;
             static int property = 20;
 
             nk_layout_row_static(ctx, 30, 80, 1);
-            if (nk_button_label(ctx, "button"))
-                fprintf(stdout, "button pressed\n");
-            nk_layout_row_dynamic(ctx, 30, 2);
-            if (nk_option_label(ctx, "easy", op == EASY)) op = EASY;
-            if (nk_option_label(ctx, "hard", op == HARD)) op = HARD;
-            nk_layout_row_dynamic(ctx, 25, 1);
-            nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
-
-            nk_layout_row_dynamic(ctx, 20, 1);
-            nk_label(ctx, "background:", NK_TEXT_LEFT);
-            nk_layout_row_dynamic(ctx, 25, 1);
-            if (nk_combo_begin_color(ctx, nk_rgb_cf(bg), nk_vec2(nk_widget_width(ctx),400))) {
-                nk_layout_row_dynamic(ctx, 120, 1);
-                bg = nk_color_picker(ctx, bg, NK_RGBA);
-                nk_layout_row_dynamic(ctx, 25, 1);
-                bg.r = nk_propertyf(ctx, "#R:", 0, bg.r, 1.0f, 0.01f,0.005f);
-                bg.g = nk_propertyf(ctx, "#G:", 0, bg.g, 1.0f, 0.01f,0.005f);
-                bg.b = nk_propertyf(ctx, "#B:", 0, bg.b, 1.0f, 0.01f,0.005f);
-                bg.a = nk_propertyf(ctx, "#A:", 0, bg.a, 1.0f, 0.01f,0.005f);
-                nk_combo_end(ctx);
-            }
+            nk_label(ctx, "Inventory", NK_TEXT_LEFT);
+            nk_layout_row_dynamic(ctx, 30, 1);
         }
         nk_end(ctx);
 
